c10ee4265383c2f8d15ff5497f76af85bbad38ef
Mike Boss <mboss@ethz.ch>
2022-05-11 11:14:30 +0200

    Naip download

--- torchgeo/datasets/naip.py
+++ torchgeo/datasets/naip.py
@@ -3,11 +3,15 @@
 
 """National Agriculture Imagery Program (NAIP) dataset."""
 
-from typing import Any, Dict, Optional
+import os
+from typing import Any, Callable, Dict, Optional, Union
 
 import matplotlib.pyplot as plt
+from rasterio.crs import CRS
+from shapely.geometry import Polygon
 
 from .geo import RasterDataset
+from .utils import BoundingBox, download_url, search_stac
 
 
 class NAIP(RasterDataset):
@@ -31,9 +35,10 @@ class NAIP(RasterDataset):
 
     # https://www.nrcs.usda.gov/Internet/FSE_DOCUMENTS/nrcs141p2_015644.pdf
     # https://planetarycomputer.microsoft.com/dataset/naip#Storage-Documentation
-    filename_glob = "m_*.*"
+    filename_glob = "*m_*.*"
     filename_regex = r"""
-        ^m
+        ^(?:(?P<state>[a-z]+)_)?
+        m
         _(?P<quadrangle>\d+)
         _(?P<quarter_quad>[a-z]+)
         _(?P<utm_zone>\d+)
@@ -46,6 +51,79 @@ class NAIP(RasterDataset):
     # Plotting
     all_bands = ["R", "G", "B", "NIR"]
     rgb_bands = ["R", "G", "B"]
+
+    def __init__(
+        self,
+        root: str = "data",
+        crs: Optional[CRS] = None,
+        res: Optional[float] = None,
+        transforms: Optional[Callable[[Dict[str, Any]], Dict[str, Any]]] = None,
+        cache: bool = True,
+        area_of_interest: Optional[Union[BoundingBox, Polygon]] = None,
+        time_range: Optional[str] = None,
+        download: bool = True,
+    ) -> None:
+        """Initialize a new Dataset instance.
+
+        Args:
+            root: root directory where dataset can be found
+            crs: :term:`coordinate reference system (CRS)` to warp to
+                (defaults to the CRS of the first file found)
+            res: resolution of the dataset in units of CRS
+                (defaults to the resolution of the first file found)
+            transforms: a function/transform that takes an input sample
+                and returns a transformed version
+            cache: if True, cache file handle to speed up repeated sampling
+            area_of_interest: BoundingBox or Polygon of interest
+            time_range: Range of time to search in
+            download: if True, download dataset and store it in the root directory
+
+        Raises:
+            FileNotFoundError: if no files are found in ``root``
+            RuntimeError: if ``download=False`` but dataset is missing or checksum fails
+        """
+        self.root = root
+        self.area_of_interest = area_of_interest
+        self.time_range = time_range
+        self.download = download
+
+        self._verify()
+
+        super().__init__(root, crs, res, transforms, cache)
+
+    def _verify(self) -> None:
+        """Verify the integrity of the dataset.
+
+        Raises:
+            RuntimeError: if ``download=False`` but dataset is missing or checksum fails
+        """
+        # Check if the zip file has already been downloaded
+        if self.area_of_interest is None:
+            return
+
+        if self.time_range is None:
+            raise RuntimeError(
+                "`area_of_interest` specificed but no `time_range` provided."
+            )
+
+        items = search_stac("naip", self.area_of_interest, self.time_range)
+
+        for item in items:
+            filename = f"{item.id}.tif"
+            url = item.get_assets()["image"].href
+
+            if os.path.exists(os.path.join(self.root, filename)):
+                continue
+
+            # Check if the user requested to download the dataset
+            if not self.download:
+                raise RuntimeError(
+                    f"{filename} not found in `root={self.root}` and `download=False`, "
+                    "either specify a different `root` directory"
+                    " or use `download=True` to automaticaly download the dataset."
+                )
+
+            download_url(url, self.root, filename)
 
     def plot(
         self,
