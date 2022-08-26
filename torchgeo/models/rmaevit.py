13e3cd32a0fd44720d866c9bd30ef88b40b17149
Mike Boss <mboss@ethz.ch>
2022-08-26 12:53:48 +0200

    Restructuring

--- torchgeo/models/rmaevit.py
+++ torchgeo/models/rmaevit.py
@@ -1,61 +1,17 @@
 """MaskedVit."""
 
-from functools import lru_cache
-from typing import cast
+from typing import cast, Any
 
 import torch
 from kornia.contrib.vit import FeedForward, TransformerEncoderBlock
 from torch import Tensor
-from torch.nn import Conv2d, LayerNorm, Linear, Module, Sequential, init
+from torch.nn import Conv2d, LayerNorm, Module, Sequential
 from torch.nn.parameter import Parameter
 
-from .utils import get_1d_sincos_pos_embed_from_grid, get_2d_sincos_pos_embed
+from .utils import _init_weights, get_channel_encodings, get_positional_encodings
 
 IN_CHANNELS = {"sentinel2": {"all": 10}, "naip": {"all": 4}}
 NUM_CLASSES = {"sentinel2": 17, "naip": 0}
-
-
-def _init_weights(m: Module) -> None:
-    """Initialize the weights."""
-    if isinstance(m, Linear):
-        init.xavier_uniform_(m.weight)
-        if isinstance(m, Linear) and m.bias is not None:
-            init.constant_(m.bias, 0)
-    elif isinstance(m, LayerNorm):
-        init.constant_(m.bias, 0)
-        init.constant_(m.weight, 1.0)
-    elif isinstance(m, Conv2d):
-        w = m.weight.data
-        init.xavier_uniform_(w.view([w.shape[0], -1]))
-
-
-def get_positional_encodings(
-    embed_dim: int, num_patches: int, channel_wise: bool
-) -> Parameter:
-    """Initialize the positional embeddings."""
-    positional_embeddings = get_2d_sincos_pos_embed(
-        embed_dim, int(num_patches**0.5), cls_token=False
-    )
-    if not channel_wise:
-        positional_embeddings = positional_embeddings.unsqueeze(0)
-
-    return Parameter(positional_embeddings, requires_grad=False)
-
-
-@lru_cache(128)
-def get_channel_encodings(
-    channels: tuple[int], num_patches: int, embed_size: int, device: str | torch.device
-) -> Tensor:
-    """Get the channel encodings for the given channels."""
-    channel_encoding = get_1d_sincos_pos_embed_from_grid(
-        embed_size,
-        torch.tensor(channels, dtype=torch.float, device=device),
-        device=device,
-    )
-
-    channel_encoding = channel_encoding.repeat_interleave(repeats=num_patches, dim=0)
-
-    return channel_encoding
 
 
 class TransformerEncoder(Module):
@@ -99,19 +55,15 @@ class EncoderEmbedding(Module):
         """Initialize the encoder embedding module."""
         super().__init__()
 
-        self.embed_dim = embed_dim
-        self.channel_wise = channel_wise
         self.num_patches = (image_size // patch_size) ** 2
-
         self.embedder = Conv2d(
             input_dim if not channel_wise else 1,
             embed_dim,
             kernel_size=patch_size,
             stride=patch_size,
         )
-
         self.positional_encodings = get_positional_encodings(
-            self.embed_dim, self.num_patches, self.channel_wise
+            embed_dim, self.num_patches, channel_wise
         )
 
         self.apply(_init_weights)
@@ -158,11 +110,11 @@ class MaskedEncoderViT(Module):
         super().__init__()
 
         self.embed_dim = embed_dim
-
         self.embed_module = EncoderEmbedding(
             in_channels, embed_dim, patch_size, image_size, channel_wise
         )
         self.num_patches = self.embed_module.num_patches
+
         self.encoder = TransformerEncoder(
             embed_dim, depth, num_heads, dropout_rate, dropout_attn
         )
@@ -171,16 +123,16 @@ class MaskedEncoderViT(Module):
         self.positional_encodings = get_positional_encodings(
             self.embed_dim, self.num_patches, channel_wise
         )
-
         self.embed_token = Parameter(
-            -torch.ones(1, self.num_patches, embed_dim), requires_grad=False
+            -torch.ones(1, self.num_patches, self.embed_dim), requires_grad=False
         )
         self.embed_token += get_positional_encodings(  # type: ignore
-            embed_dim, self.num_patches, channel_wise
+            self.embed_dim, self.num_patches, channel_wise
         )
 
         self.apply(_init_weights)
 
+    # TODO: Rework this to be more efficient.
     def get_embed_token(self, x: Tensor, mask: Tensor, embed_token: Tensor) -> Tensor:
         """Reduce the embed token by using the values not masked in place."""
         PS = len(mask)
@@ -204,10 +156,13 @@ class MaskedEncoderViT(Module):
 
         return embed_token
 
-    def forward(self, item: dict[str, Tensor]) -> Tensor:
+    def forward(self, item: dict[str, Tensor | list[int]]) -> Tensor:
         """Forward pass of the model."""
-        x = self.embed_module(item["input"], item["encoder_channels"])
-        mask = item.get("mask", None)
+        x = cast(Tensor, item["input"])
+        channels = cast(list[int], item.get("encoder_channels", []))
+        mask = cast(Tensor | None, item.get("mask", None))
+
+        x = self.embed_module(x, channels)
 
         if mask is not None:
             x = x[:, ~mask]
@@ -215,14 +170,14 @@ class MaskedEncoderViT(Module):
         x = self.encoder(x)
         x = self.norm(x)
 
-        if mask is None:
-            x = x[:, : self.num_patches]
-        else:
+        if mask is not None:
             B, *_ = x.shape
             embed_token = self.embed_token.repeat(B, 1, 1)
             x = self.get_embed_token(x, mask, embed_token)
+        else:
+            x = x[:, : self.num_patches]
 
-        return cast(Tensor, x)
+        return x
 
 
 class MaskedEmbeddingExpander(Module):
@@ -268,9 +223,8 @@ class DecoderEmbedding(Module):
     def forward(self, x: Tensor, channels: list[int] = []) -> Tensor:
         """Embed the decoder input with channel encodings."""
         if len(channels):
-            x += get_channel_encodings(
-                tuple(channels), self.num_patches, x.shape[-1], x.device
-            )
+            *_, H = x.shape
+            x += get_channel_encodings(tuple(channels), self.num_patches, H, x.device)
 
         return x
 
@@ -301,9 +255,10 @@ class MaskedDecoderViT(Module):
 
         self.apply(_init_weights)
 
-    def forward(self, item: dict[str, Tensor]) -> Tensor:
+    def forward(self, item: dict[str, Tensor | list[int]]) -> Tensor:
         """Forward pass of the model."""
-        latent, channels = item["latent"], item["decoder_channels"]
+        latent = cast(Tensor, item["latent"])
+        channels = cast(list[int], item.get("decoder_channels", []))
 
         output = []
         for channel in channels:
